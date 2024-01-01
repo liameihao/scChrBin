b2c76d24ef5618b56f6719cb42316c9c8d302057
Pengfei Liang <awd5174119@126.com>
2024-01-01 13:08:03 +0800

    updated

--- README.md
+++ README.md
@@ -3,16 +3,31 @@ Single-cell transcriptome gene expression values are binned according to chromat
 
 # Use
 ```python
-# python main.py chr_length_path gene_gtf_genename_path width counts_path
-python main.py chr_length.txt gene_gtf_genename.txt 10 counts.txt
-# chr_length_path: chr_length.txt (format:txt)
-# gene_gtf_genename: gene transfer format(GTF) (format:txt)
-# width: bin width (example:10)
-# counts_path: single cell RNA-Seq expression (format:txt)
+# example
+python main.py --chr_length_file chromelength.txt --gene_gtf_file Homo_sapiens.GRCh37.87.gtf  --width 10 --input_file rpkm.txt
+# get output file: rpkm_scChrBin_bin_counts.txt
+
+# help
+python main.py --help
+Usage: 123.py [OPTIONS]
+
+# Options:
+#   --chr_length_file PATH  Path to the chromosome length file.  [required]
+#   --gene_gtf_file PATH    Path to the gene GTF gene name file.  [required]
+#   --width INTEGER         gene bin width.  [required]
+#   --input_file PATH       Path for the input file.  [required]
+#   --help                  Show this message and exit.
 ```
 
+We recommend finding the optimal number of bins by making predictions on the data using a machine learning classifier. By utilizing the [Feature-scML](https://github.com/liameihao/Feature-scML) tool, it is possible to automate machine learning for binned data, and then proceed with further calculations using the optimal number of bins. 
+
 # Requirements
+
 - ray=1.11.0
 - pandas
 - numpy
+- click
 
+# Reference
+
+Feature-scML：**Liang P**, Wang H, Liang Y, et al. Feature-scML: An Open-source Python Package for the Feature Importance Visualization of Single-Cell Omics with Machine Learning[J]. Current Bioinformatics, 2022, 17(7): 578-585 (https://doi.org/10.2174/1574893617666220608123804).
--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import pandas as pd
 import numpy as np
 import re
 import ray
-import sys
 import os
+import click
 
 
-def gene_gtf_info(gene_length_path, gene_gtf_genename_path):
-    gene_length = pd.read_csv(gene_length_path,sep="\t")
-    gene_gtf = pd.read_csv(gene_gtf_genename_path, sep="\t", comment="#", header=None, low_memory=False)
+def gene_gtf_info(chr_length_file, gene_gtf_file):
+    gene_length = pd.read_csv(chr_length_file,sep="\t")
+    gene_gtf = pd.read_csv(gene_gtf_file, sep="\t", comment="#", header=None, low_memory=False)
     pattern = re.compile('gene_name "(.*?)";')
     gene_gtf_gene = gene_gtf[gene_gtf.iloc[:, 2] == "gene"]
     gene_gtf_gene.head()
@@ -63,26 +63,31 @@ def filename_rename(file_path,suffix):
     base_name, extension = os.path.splitext(file_path)
     return base_name+suffix+extension
 
-def main(gene_length_path, gene_gtf_genename_path, width, counts_path):
-    gene_gtf_genename, gene_length = gene_gtf_info(gene_length_path, gene_gtf_genename_path)
-    gene_length.to_csv(filename_rename(gene_length_path,"_scChrBin_gene_length"), index=None)
-    gene_gtf_genename.to_csv(filename_rename(gene_gtf_genename_path,"_scChrBin_gene_gtf_genename"), index=None)
-    #
-    gene_bin, bins_dict = bin_split(width, gene_gtf_genename, gene_length)
-    data_counts = pd.read_csv(counts_path, sep="\t")
-    data_counts.columns = ["gene_name"]+list(data_counts.columns[1:])
-    results = bin_sum(data_counts, gene_bin, width)
-    results.to_csv(filename_rename(counts_path,"_scChrBin_bin_counts"), index=None)
-
-if __name__ == '__main__':
-    gene_length_path = str(sys.argv[1])
-    gene_gtf_genename_path = str(sys.argv[2])
-    width = int(sys.argv[3])
-    counts_path = str(sys.argv[4])
-    # run
+@click.command()
+@click.option('--chr_length_file', type=click.Path(exists=True), required=True, help='Path to the chromosome length file.')
+@click.option('--gene_gtf_file', type=click.Path(exists=True), required=True, help='Path to the gene GTF gene name file.')
+@click.option('--width', type=int, required=True, help='gene bin width.')
+@click.option('--input_file', type=click.Path(exists=True), required=True, help='Path for the input file.')
+def main(chr_length_file, gene_gtf_file, width, input_file):
     print("#################")
     print("start")
-    main(gene_length_path, gene_gtf_genename_path, width, counts_path)
+    gene_gtf_genename, gene_length = gene_gtf_info(chr_length_file, gene_gtf_file)
+    gene_length.to_csv(filename_rename(chr_length_file,"_scChrBin_gene_length"), index=None)
+    gene_gtf_genename.to_csv(filename_rename(gene_gtf_file,"_scChrBin_gene_gtf_genename"), index=None)
+    #
+    gene_bin, bins_dict = bin_split(width, gene_gtf_genename, gene_length)
+    data_counts = pd.read_csv(input_file, sep="\t")
+    data_counts.columns = ["gene_name"]+list(data_counts.columns[1:])
+    results = bin_sum(data_counts, gene_bin, width)
+    results.to_csv(filename_rename(input_file,"_scChrBin_bin_counts"), index=None)
+    print("output file path: ", filename_rename(input_file,"_scChrBin_bin_counts"))
     print("end")
     print("#################")
+
+
+
+if __name__ == '__main__':
+    # run
+    main()
+    
 
